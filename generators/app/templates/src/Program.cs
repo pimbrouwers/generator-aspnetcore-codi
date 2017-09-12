9eb7f857923af778439177cd80abbc8b18f16a51
Pim Brouwers <pbrouwers@nhlpa.com>
2017-09-12 14:28:20 -0400

    ServiceCollection object renamed

--- generators/app/templates/src/Program.cs
+++ generators/app/templates/src/Program.cs
@@ -20,33 +20,33 @@ namespace <%= appName %>
         static void Main(string[] args)
         {
             //create service collection
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
+            var services = new ServiceCollection();
+            ConfigureServices(services);
 
             // create service provider
-            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var serviceProvider = services.BuildServiceProvider();
 
             //run app            
             serviceProvider.GetService<App>().Run();           
         }
         
-        private static void ConfigureServices(IServiceCollection serviceCollection)
+        private static void ConfigureServices(IServiceCollection services)
         {            
             // build configuration
             var configuration = new ConfigurationBuilder()
-                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
                                     .AddJsonFile("appsettings.json", false)
                                     .Build();
             
             //specify the instances of configuration
-            serviceCollection.AddOptions();            
-            serviceCollection.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+            services.AddOptions();            
+            services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
 
             // add services
-            serviceCollection.AddTransient<IExampleService, ExampleService>();
+            services.AddTransient<IExampleService, ExampleService>();
             
             // add app stub
-            serviceCollection.AddTransient<App>();
+            services.AddTransient<App>();
         }
     }
 }
