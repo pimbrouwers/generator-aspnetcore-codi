b903e47a9870cb8c43255bc1f35a433d8db004d6
Pim Brouwers <pbrouwers@nhlpa.com>
2017-10-27 10:11:35 -0400

    reflection assembly added to program.cs

--- generators/app/templates/src/Program.cs
+++ generators/app/templates/src/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using <%= appName %>.Services;
 using System.IO;
+using System.Reflection;
 
 namespace <%= appName %>
 {    
